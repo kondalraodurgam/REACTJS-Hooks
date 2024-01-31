9dbe41ccc88730e03a3b4b9b819a45f6544b7d10
Kondal Durgam <kondaldurgam733@gmail.com>
2024-01-31 20:57:35 +0530

    Added Sample Counte example
    Added sample counter-example class component

---
+++ Counter.js
@@ -0,0 +1,30 @@
+import React,{Component} from "react";
+
+class Counter extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count : 0
+        }
+    }
+    incrementValue(){
+        this.setState((prevState)=>({
+            count : prevState.count + 1
+        }))
+    }
+    decrementValue (){
+        this.setState((prevState)=>({
+            count : prevState.count -1
+        }))
+    }
+    render(){
+        return(
+            <div>
+                <h1>Count Value : {this.state.count}</h1>
+                <button onClick={()=>this.incrementValue()}>Increment </button>
+                <button onClick={()=>this.decrementValue()}>Decreement</button>
+            </div>
+        )
+    }
+}
+export default Counter;
