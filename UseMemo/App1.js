37f6930d10850a34330d55f69a9067acb9ab8294
Kondal Durgam <kondaldurgam733@gmail.com>
2024-03-14 18:08:17 +0530

    Create App1.js

---
+++ UseMemo/App1.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { createTodos } from './utils.js';
+import TodoList from './TodoList.js';
+
+const todos = createTodos();
+// console.log(todos, 'Kondal');
+export default function App() {
+  const [tab, setTab] = useState('all');
+  const [isDark, setIsDark] = useState(false);
+  return (
+    <>
+      <button onClick={() => setTab('all')}>All</button>
+      <button onClick={() => setTab('active')}>Active</button>
+      <button onClick={() => setTab('completed')}>Completed</button>
+      <br />
+      <label>
+        <input
+          type="checkbox"
+          checked={isDark}
+          onChange={(e) => setIsDark(e.target.checked)}
+        />
+        Dark mode
+      </label>
+      <hr />
+      <TodoList todos={todos} tab={tab} theme={isDark ? 'dark' : 'light'} />
+    </>
+  );
+}
