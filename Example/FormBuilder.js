f850fc3ac181a5cfbd439d11e2eafd89fe52dead
Kondal Durgam <kondaldurgam733@gmail.com>
2025-01-07 05:31:45 +0000

    Added the FormBuilder

---
+++ Example/FormBuilder.js
@@ -0,0 +1,31 @@
+import React, { useState,memo} from "react";
+
+const Field = memo(({ field, updateField }) => (
+  <input
+    type={field.type}
+    placeholder={field.placeholder}
+    value={field.value}
+    onChange={(e) => updateField(field.id, e.target.value)}
+  />
+));
+
+const FormBuilder = () => {
+  const [fields, setFields] = useState([]);
+
+  const addField = () => setFields([...fields, { id: Date.now(), type: "text", value: "" }]);
+
+  const updateField = (id, value) => {
+    setFields(fields.map((f) => (f.id === id ? { ...f, value } : f)));
+  };
+
+  return (
+    <div>
+      {fields.map((field) => (
+        <Field key={field.id} field={field} updateField={updateField} />
+      ))}
+      <button onClick={addField}>Add Field</button>
+    </div>
+  );
+};
+
+export default FormBuilder;
